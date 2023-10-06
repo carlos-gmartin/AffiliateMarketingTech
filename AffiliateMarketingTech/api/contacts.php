84c0840a849f26e0365b6793c548a30f11fb7725
Carlos Gerardo Martin <cm43833@gmail.com>
2023-10-06 16:31:03 +0100

    Added contacts.php page

--- AffiliateMarketingTech/api/contacts.php
+++ AffiliateMarketingTech/api/contacts.php
@@ -1,29 +1,38 @@
 <?php
-$host = "localhost"; 
-$user = "affilia5_carlos"; 
-$password = "1&)-f)j)k6n,"; 
-$dbname = "affilia5_main"; 
+  $host = "localhost"; 
+  $user = "affilia5_carlos"; 
+  $password = "1&)-f)j)k6n,"; 
+  $dbname = "affilia5_main"; 
 
-$con = mysqli_connect($host, $user, $password,$dbname);
+  $con = mysqli_connect($host, $user, $password,$dbname);
 
 
-if (!$con) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-else{
-    echo "Success!";
+  if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  else{
+      echo "Success!";
 
-    $name = $_POST['name'];
-    $business_name = $_POST['business_name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $platform = $_POST['platform'];
-    $page_url = $_POST['page_url'];
-    $contact_method = $_POST['contact_method'];
-    $note = $_POST['note'];
+      $name = $_POST['name'];
+      $business_name = $_POST['business_name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $platform = $_POST['platform'];
+      $page_url = $_POST['page_url'];
+      $contact_method = $_POST['contact_method'];
+      $note = $_POST['note'];
 
-    $sql = "INSERT INTO `request`(`name`, `business_name`, `email`, `phone`, `platform`, `page_url`, `contact_method`, `note`) VALUES ('$name','$business_name','$email','$phone','$platform','$page_url','$contact_method','$note')"
-    
-}
+      $sql = "INSERT INTO `request`(`name`, `business_name`, `email`, `phone`, `platform`, `page_url`, `contact_method`, `note`) VALUES ('$name','$business_name','$email','$phone','$platform','$page_url','$contact_method','$note')";
 
+
+      $res = mysqli_query($con, $sql);
+
+      if ($res){
+        echo "success!";
+      }
+      else{
+        echo "Error!";
+      }
+      $con->close();
+  }
 ?>
